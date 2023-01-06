e375824a9bffdf37f1c0d7b76d68d9b6fd476478
Hasibull <hasibul.sbn@gmail.com>
2023-01-07 00:01:31 +0600

    solved

--- Problem solving/Codeforces/Random solve/1772_C.py
+++ Problem solving/Codeforces/Random solve/1772_C.py
@@ -10,5 +10,16 @@ while tc > 0:
     a = [1] # this will be the final list
     ct = 1
 
-    for i in range(1, k):
-        if (i+ct) <= n and ()
+    for i in range(k-1):
+        if (a[i]+ct) <= n and (n-(a[i]+ct)) >= (k-i-2):
+            a.insert(i+1, a[i] + ct)
+            ct += 1
+        else:
+            a.insert(i+1, a[i] + 1)
+    
+    for val in a:
+        print(val, end=' ')
+    print()
+
+    tc -= 1
+
